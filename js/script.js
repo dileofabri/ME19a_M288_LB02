bc0fa20794c7c7cc8a101444b344a5a34e9910fc
Fabrizio Di Leo <dileo.fabri@gmail.com>
2020-11-21 16:39:01 +0100

    LB02 initialisiert

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let actualTemp = 10;
+if (actualTemp > 15) {
+    document.getElementById('info').innerText = "It's warm.";
+} else {
+    document.getElementById('info').innerText = "It's cold.";
+}
