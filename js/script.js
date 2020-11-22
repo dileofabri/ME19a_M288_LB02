39014a798ca4f9e4438d5038f806e6aa77fe94be
Fabrizio Di Leo <dileo.fabri@gmail.com>
2020-11-22 10:25:49 +0100

    HTML Dateien aktualisiert

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
-let actualTemp = 10;
-if (actualTemp > 15) {
-    document.getElementById('info').innerText = "It's warm.";
-} else {
-    document.getElementById('info').innerText = "It's cold.";
+function getBalance() {
+    /* var elMsg = document.getElementById('result');
+    var elAddress = document.getElementById('public-address');
+    if (elAddress.value.length < 26) {
+        elMsg.textContent = 'Wrong!';
+    } else {
+        elMsg.textContent = '';
+    } */
+    var myJSON = {"final_balance":""};
+    var myObj = JSON.parse(myJSON);
+    document.getElementById("demo").innerHTML = myObj.name;
 }
