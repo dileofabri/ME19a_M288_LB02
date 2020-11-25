cba4e6e41a998c9bdbd6aa41d0362f7d94feb8c1
Fabrizio Di Leo <dileo.fabri@gmail.com>
2020-11-25 09:25:19 +0100

    Fehlerbehebungen am Stylesheet, HTML und Script

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
 function getBalance() {
-    /* var elMsg = document.getElementById('result');
-    var elAddress = document.getElementById('public-address');
-    if (elAddress.value.length < 26) {
-        elMsg.textContent = 'Wrong!';
-    } else {
-        elMsg.textContent = '';
-    } */
-    var myJSON = {"final_balance":""};
-    var myObj = JSON.parse(myJSON);
-    document.getElementById("demo").innerHTML = myObj.name;
+
 }
