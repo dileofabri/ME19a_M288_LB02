4976c312c619c633fd4826635cb5131bfa447819
Fabrizio Di Leo <dileo.fabri@gmail.com>
2020-11-25 11:10:30 +0100

    Einige kleine Änderungen am CSS, HTML und JS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
-function getBalance() {
+function setColor() {
+    let selectedColor = document.getElementById('input').value;
+    changeColor(selectedColor);
+}
 
+function changeColor() {
+    document.body.style.backgroundColor;
 }
