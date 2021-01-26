1e0e44c5ca1c32189e74bb37e7a332ce0d4940d0
Fabrizio Di Leo <dileo.fabri@gmail.com>
2021-01-26 12:37:42 +0100

    Slideshow added, script changed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-function chooseColor() {
-    let selectedColor = document.getElementById('input').value;
-    document.body.style.backgroundColor = selectedColor;
-    location.reload();
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
 }
